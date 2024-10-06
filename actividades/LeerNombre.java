72899ae29be6be2056a79a07c07579c5e8ceed88
AlvaroQP <alonsol1569@gmail.com>
2024-10-06 12:40:49 +0200

    Actividad 1.4 hecha

--- actividades/LeerNombre.java
+++ actividades/LeerNombre.java
@@ -1,21 +1,16 @@
 package actividades;
 
 import java.io.IOException;
-// import java.util.Scanner;
 
 public class LeerNombre {
     public static void main(String[] args) throws IOException {
         // Actividad 1.4
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Dime un nombre");
-        // String nombre = sc.nextLine();
         if (args[0].equals("Alvaro")) {
-            System.out.println("Nombre valido");
+            System.out.print("Nombre valido");
             System.exit(1);
         } else {
             System.out.println("El nombre no es valido");
             System.exit(-1);
         }
-        // sc.close();
     }
 }
