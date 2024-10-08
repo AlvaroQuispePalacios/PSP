24f6e82c0a04ecf3e1987bbc8b50b473a14aa9cd
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-10-08 15:22:50 +0200

    sumadores y ficheros casi hecho

--- sumadoresyficheros/SumadoresYFicheros.java
+++ sumadoresyficheros/SumadoresYFicheros.java
@@ -1,25 +1,55 @@
 package sumadoresyficheros;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Scanner;
 
 public class SumadoresYFicheros {
 
     /*
-        Crear un programa que permita parametrizar el lanzamiento de sumadores, que vuelque el contenido de las sumas en ficheros y que permita al programa principal recuperar las sumas de los ficheros parciales. 
-    */
+     * Crear un programa que permita parametrizar el lanzamiento de sumadores, que
+     * vuelque el contenido de las sumas en ficheros y que permita al programa
+     * principal recuperar las sumas de los ficheros parciales.
+     */
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Dime un numero");
-        int x = sc.nextInt();
+        String x = sc.nextLine();
         System.out.println("Dime el otro numero");
-        int y = sc.nextInt();
-        ProcessBuilder pb = new ProcessBuilder("java", "sumadoresyficheros\\Suma.java");
-        Process p = pb.start();
-        OutputStream os = p.getOutputStream();
-        os.write(y);
-        sc.close();
+        String y = sc.nextLine();
+        
+        try {
+            // 
+            ProcessBuilder pb = new ProcessBuilder("java", "sumadoresyficheros\\Suma.java", x, y);
+            guardarSuma(pb);
+            Process p = pb.start();
+            sc.close();
+            
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+       public static void guardarSuma(ProcessBuilder pb){
+        // Creamos el directorio donde se guardaran las sumas
+        File directorio = new File(".\\sumadoresyficheros\\sumas");
+        if(!directorio.exists()){
+            directorio.mkdir();
+        }
+
+        int contador = 1;
+        boolean salir = false;
+
+        do{
+            File f = new File(".\\sumadoresyficheros\\sumas\\suma" + contador + ".txt");
+            // Si el fichero ya existe suma uno al contador hasta que un fichero sea valido
+            if(!f.exists()){
+                pb.redirectOutput(f);
+                salir = true;
+            }else{
+                contador++;
+            }
+        }while(!salir);
 
     }
 }
