c0701c19266035df896fd8d956ee9c4a59de2250
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-10-17 17:03:14 +0200

    sumadores y fucheros menu

--- sumadoresyficheros/SumadoresYFicheros.java
+++ sumadoresyficheros/SumadoresYFicheros.java
@@ -13,43 +13,69 @@ public class SumadoresYFicheros {
      */
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dime un numero");
-        String x = sc.nextLine();
-        System.out.println("Dime el otro numero");
-        String y = sc.nextLine();
-        
+        boolean salir = false;
+        // System.out.println("Dime un numero");
+        // String x = sc.nextLine();
+        // System.out.println("Dime el otro numero");
+        // String y = sc.nextLine();
+
         try {
-            // 
-            ProcessBuilder pb = new ProcessBuilder("java", "sumadoresyficheros\\Suma.java", x, y);
-            guardarSuma(pb);
-            Process p = pb.start();
+            //
+            // ProcessBuilder pb = new ProcessBuilder("java",
+            // "sumadoresyficheros\\Suma.java", x, y);
+            // guardarSuma(pb);
+            // Process p = pb.start();
+            do {
+                System.out.println("1. Sumar dos numeros\n2. Recuperar Suma\n3. Salir");
+                int respuesta = sc.nextInt();
+
+                switch (respuesta) {
+                    case 1:
+                        System.out.println("Dime un numero");
+                        String x = sc.nextLine();
+                        System.out.println("Dime el otro numero");
+                        String y = sc.nextLine();
+                        ProcessBuilder pb = new ProcessBuilder("java", "sumadoresyficheros\\Suma.java", x, y);
+                        guardarSuma(pb);
+                        Process p = pb.start();
+                        break;
+                    case 2:
+                        System.out.println("Recuperar dos numeros");
+                        break;
+                    case 3:
+                        salir = true;
+                    default:
+                        break;
+                }
+            } while (salir != true);
+
             sc.close();
-            
+
         } catch (Exception e) {
             e.getStackTrace();
         }
     }
 
-       public static void guardarSuma(ProcessBuilder pb){
+    public static void guardarSuma(ProcessBuilder pb) {
         // Creamos el directorio donde se guardaran las sumas
         File directorio = new File(".\\sumadoresyficheros\\sumas");
-        if(!directorio.exists()){
+        if (!directorio.exists()) {
             directorio.mkdir();
         }
 
         int contador = 1;
         boolean salir = false;
 
-        do{
+        do {
             File f = new File(".\\sumadoresyficheros\\sumas\\suma" + contador + ".txt");
             // Si el fichero ya existe suma uno al contador hasta que un fichero sea valido
-            if(!f.exists()){
+            if (!f.exists()) {
                 pb.redirectOutput(f);
                 salir = true;
-            }else{
+            } else {
                 contador++;
             }
-        }while(!salir);
+        } while (!salir);
 
     }
 }
