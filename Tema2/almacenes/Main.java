19cdcdde9d3bacea08abe5185b7810d32cea1685
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-11-05 17:24:21 +0100

    casi

--- Tema2/almacenes/Main.java
+++ Tema2/almacenes/Main.java
@@ -3,11 +3,19 @@ package Tema2.almacenes;
 public class Main {
 
     public static class Cliente extends Thread {
+        private int nCliente;
         private int intentos = 0;
         private Contenedor contenedor;
 
+        public Cliente(int nCliente, Contenedor c) {
+            this.nCliente = nCliente;
+            this.contenedor = c;
+        }
+
         public void run() {
-            this.intentos = contenedor.obtenerProducto(intentos);
+            // System.out.println("Cliente: " + this.nCliente +"\t" + this.intentos);
+            this.intentos = contenedor.intentarPasarPuerta(this.intentos);
+            System.out.println(intentos);
         }
     }
 
@@ -15,35 +23,77 @@ public class Main {
         private int productos = 100;
         private boolean alguienDentro = false;
 
-        public synchronized int obtenerProducto(int intentos) {
-            if (intentos != 10) {
-                while (alguienDentro) {
-                    try {
-                        // Si alguien esta dentro esperamos
-                        wait();
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                    }
-                }
-                // Si no hay nadie dentro entramos
-                alguienDentro = true;
-                if (productos > 0) {
-                    productos--;
-                } else {
+
+        // Probar en crear dos metodos uno que siempre intente pasar y otro que vea si hay alguien dentro o sea el cliente que avise que esta dentro
+        // Intentar pasar puerta
+        public synchronized int intentarPasarPuerta(int intentos) {
+        while (intentos < 0) {
+            if(alguienDentro){
+                try {
+                    System.out.println("esperando");
                     intentos++;
-                }
-                alguienDentro = false;
-                notifyAll();
+                    wait();
+                } catch (Exception e) {}
             }else{
-                // Ya no entra
+
             }
-            return intentos;
+
+        }    
+        return intentos;
+
+            
+            // if (intentos < 10) {
+            //     while (alguienDentro == true) {
+            //         try {
+            //             // Si alguien esta dentro esperamos
+            //             System.out.println("esperando");
+            //             intentos++;
+            //             wait();
+            //         } catch (Exception e) {
+
+            //         }
+            //     }
+
+            //     // alguienDentro = true;
+            //     // if (productos > 0) {
+            //     //     productos--;
+            //     // } else {
+            //     //     // El cliente se va
+            //     // }
+            //     // alguienDentro = false;
+            //     // notifyAll();
+
+            //     // // Si no hay nadie dentro entramos
+            // }
+
+            
+            // alguienDentro = true;
+            // if (productos > 0) {
+            //     productos--;
+            // } else {
+            //     // El cliente se va
+            // }
+            // alguienDentro = false;
+            // notifyAll();
+
+            // // Si no hay nadie dentro entramos
+            // System.out.println(intentos);
+            // return intentos;
+
+            // System.out.println(productos);
         }
+
+        
+
+
+
 
     }
 
     public static void main(String[] args) {
-        Cliente cliente = new Cliente();
-        Contenedor contenedor = new Contenedor(cliente.intentos);
+        Contenedor c = new Contenedor();
+        for (int i = 1; i <= 300; i++) {
+            new Cliente(i, c).start();
+        }
     }
 }
