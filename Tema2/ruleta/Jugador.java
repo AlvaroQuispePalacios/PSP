36e8c238413f60d08fa81fbe3baf5eb4237c6178
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-11-12 17:20:57 +0100

    Inicio de actividad Ruleta

---
+++ Tema2/ruleta/Jugador.java
@@ -0,0 +1,9 @@
+package Tema2.ruleta;
+
+public class Jugador implements Runnable{
+    Ruleta ruleta;
+    int dinero = 1000;
+    public void run(){
+
+    }
+}
---
+++ Tema2/ruleta/Ruleta.java
@@ -0,0 +1,13 @@
+package Tema2.ruleta;
+
+public class Ruleta {
+    int dinero = 50000;
+    int numeroActualRuleta;
+
+    public synchronized void girarRuleta(){
+
+    }
+
+    
+    // girarRuleta() cada 3seg
+}
