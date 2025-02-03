caad7c4445872171284cd9aadc141e8cdbbfbbea
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2025-02-03 19:39:11 +0100

    03-02-2025 actividad 4_3

--- Tema4/Actividad4_2/src/Main.java
+++ Tema4/Actividad4_2/src/Main.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class Main extends JFrame {
     private JPanel MainPanel;
+    private JButton btnDescargar;
+    private JButton btnSalir;
+    private JPanel pContenido;
+    private JList listaDirectorios;
 
     public Main(){
         setTitle("Actividad 4.2");
@@ -10,9 +16,23 @@ public class Main extends JFrame {
         setVisible(true);
         setContentPane(MainPanel);
         setResizable(false);
+
+        //
+        DefaultListModel xd = new DefaultListModel();
+        xd.addElement(new String("a"));
+        xd.addElement(new String("b"));
+        listaDirectorios.setModel(xd);
+        listaDirectorios.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                System.out.println(e.getFirstIndex());
+            }
+        });
+
     }
 
     public static void main(String[] args) {
         new Main();
+
     }
 }
