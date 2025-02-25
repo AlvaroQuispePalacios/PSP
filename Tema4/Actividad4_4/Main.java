19c4081a765f12785b9db264e0cc903187592faa
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2025-02-25 17:16:51 +0100

    Actividad 4.4 en prceso

--- Tema4/Actividad4_4/Main.java
+++ Tema4/Actividad4_4/Main.java
@@ -6,53 +6,62 @@ import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 
 import org.apache.commons.net.smtp.AuthenticatingSMTPClient;
+import org.apache.commons.net.smtp.SMTP;
+import org.apache.commons.net.smtp.SMTPClient;
+import org.apache.commons.net.smtp.SMTPReply;
 
 public class Main {
+    static SMTPClient cliente = new SMTPClient();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-
-        AuthenticatingSMTPClient cliente = new AuthenticatingSMTPClient();
-
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Introduce el servidor SMTP: ");
-        String server = sc.nextLine();
-
-        System.out.println("Necesita negociación TLS (S/N)");
-        char tls = sc.next().trim().toUpperCase().charAt(0);
-
-
-
-        System.out.println("Dime el usuario");
-        String usuario = sc.nextLine();
-
-        System.out.println("Dime la contraseña");
-        String pwd = sc.nextLine();
-
-        System.out.println("Dime el puerto");
-        int puerto = sc.nextInt();
-
-        System.out.println("Correo del remitente");
-        String correoRemitente = sc.nextLine();
-
-        System.out.println("Correo del destinatario");
-        String correoDestinatario = sc.nextLine();
-
-        System.out.println("Introduce el asunto");
-        String asunto = sc.nextLine();
-
-
-        System.out.println("Introduce el mensaje, finalizará cuando se introduzca un *");
-    
-
         try {
-            int respuesta;
-            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            kmf.init(null, null);
-            KeyManager km = kmf.getKeyManagers()[0];
+            System.out.print("Introduce el servidor SMTP: ");
+            String server = sc.nextLine().trim();
+
+            System.out.print("Necesita negociación TLS (S/N): ");
+            char tls = sc.next().trim().toUpperCase().charAt(0);
+            sc.nextLine();
+
+            System.out.print("Dime el usuario: ");
+            String usuario = sc.nextLine();
+
+            System.out.print("Dime la contraseña: ");
+            String pwd = sc.nextLine();
+
+            System.out.print("Dime el puerto: ");
+            int puerto = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Correo del remitente: ");
+            String correoRemitente = sc.nextLine();
+
+            System.out.print("Correo del destinatario: ");
+            String correoDestinatario = sc.nextLine();
+
+            System.out.print("Introduce el asunto: ");
+            String asunto = sc.nextLine();
+
+            System.out.println("Introduce el mensaje, finalizará cuando se introduzca un *: ");
+            String mensaje = sc.nextLine();
             
+            cliente.connect(server, puerto);
+            int respuesta = cliente.getReplyCode();
+
+            if(!SMTPReply.isPositiveCompletion(respuesta)){
+                System.out.println("Conexión rechazada");
+                cliente.disconnect();
+            }
+
+            cliente.login();
 
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println("Error al introducir datos: " + e.getMessage());
+
         }
+
     }
+
+
+
 }
